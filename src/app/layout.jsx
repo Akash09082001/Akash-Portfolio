f59378c13301265dbf1467b58cef6beb6a16e71f
Akash09082001 <aakashl@letsupgrade.in>
2024-06-30 19:46:36 +0530

    chore: Initial Setup Done

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "Akash Lakade Portfolio",
+    description: "Front End Devloper",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
